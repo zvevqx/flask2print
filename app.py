0b89a77e0d0ba690e70783b8384ef359f851acde
erreur401 <erreur401@gmail.com>
2022-03-25 15:04:51 +0100

    single article pdf genrator ans md uploader added

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_weasyprint import HTML, render_pdf
-from flask import render_template
+from flask import render_template , request
+from flask import flash, redirect, url_for
 from flask_flatpages import FlatPages
+from werkzeug.utils import secure_filename
 import os
 
 app = Flask(__name__)
@@ -14,6 +16,9 @@ FLATPAGES_EXTENSION_CONFIGS = {
     }
 }
 
+UPLOAD_FOLDER = 'pages/'
+ALLOWED_EXTENSIONS = {'md', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 app.config.from_object(__name__)
@@ -34,6 +39,12 @@ def index():
     return render_template('text.html', articles=latest) 
 
 
+@app.route('/<path:path>/')
+def pdfExportsingle(path):
+    page = pages.get_or_404(path)
+    html =  render_template('single.html', page=page) 
+    return render_pdf(HTML(string=html))
+
 
 @app.route('/pdf')
 def pdfExport():
@@ -42,4 +53,36 @@ def pdfExport():
     html =  render_template('text.html', articles=latest) 
     return render_pdf(HTML(string=html))
 
-        
+       
+ 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('upload_file',
+                                    filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
